9b18ee4a4d85a48ae1e5aca4e5d733d3afaf7d2a
Lakshraj99 <lakshraaj99@gmail.com>
2026-03-10 20:02:24 +0530

    Integrate FRED API for risk-free rate data

--- scripts/api_ingestion.R
+++ scripts/api_ingestion.R
@@ -54,3 +54,20 @@ head(stock_prices)
 
 # Save dataset locally
 write.csv(stock_prices, "data/stock_prices.csv", row.names = FALSE)
+library(fredr)
+
+# Set your FRED API key
+fredr_set_key("d730817f3fe07cca979811852a4c20b6")
+
+# Fetch 3-Month Treasury Bill rate
+risk_free_rate <- fredr(
+  series_id = "DTB3",
+  observation_start = as.Date("2019-01-01"),
+  observation_end = Sys.Date()
+)
+
+# Preview data
+head(risk_free_rate)
+
+# Save dataset
+write.csv(risk_free_rate, "data/risk_free_rate.csv", row.names = FALSE)
